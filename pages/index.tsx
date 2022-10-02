da4360331b478d726bfdf36bc262487eef79dd41
sid <sidharthad@protonmail.com>
2022-10-02 17:40:15 -0400

    added hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import Header from '../components/landing/Header.tsx';
+import Hero from '../components/landing/Hero.tsx';
 
 const Home: NextPage = () => {
   return (
-		<Header />
+		<Hero />
   )
 }
 
